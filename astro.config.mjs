188c476f5665db1633c2bfb9f71b4b6c9533fefa
kristo <kristoferjussmann@gmail.com>
2025-11-06 01:35:25 +0200

    Set Astro base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ker102.github.io/Astroblogauto/',
+	base: '/Astroblogauto',
 	integrations: [mdx(), sitemap()],
 });
